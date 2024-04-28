12cb024e886529be012c992536b76a7925646d6f
Dipesh K. Shrestha <dipeshjhon098@gmail.com>
2024-04-28 20:31:02 +0545

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,11 +20,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Dipesh K. Shrestha| Official Website";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Dipesh K. Shrestha| Official Website ";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
